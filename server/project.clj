280a7da8ecedba0fa81780316135a69b1a5f937b
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-09-24 22:51:03 +0100

    Some possibly functional server.

--- server/project.clj
+++ server/project.clj
@@ -2,7 +2,8 @@
   :description "A simple server to enable WebRTC connections between browsers."
   :url "https://github.com/eutropius225/kutyagumi"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.java-websocket/Java-WebSocket "1.5.1"]]
   :repl-options {:init-ns server.core}
   :main server.core
   :aot [server.core])
--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -1,61 +1,145 @@
 (ns server.core
   (:gen-class)
-  (:require [clojure.java.io :as io]
-            [clojure.edn :as edn])
-  (:import (java.net ServerSocket SocketException Socket)
-           (clojure.lang LineNumberingPushbackReader)
-           (java.io IOException Writer)))
+  (:require [clojure.edn :as edn])
+  (:import (org.java_websocket.server WebSocketServer)
+           (java.net InetSocketAddress)
+           (org.java_websocket WebSocket)))
 
-(def PORT 4892)
+(defn send-to [^WebSocket socket packet]
+  (.send socket (pr-str packet)))
 
-(defmacro thread [& body]
-  `(.start
-     (Thread.
-       ^Runnable
-       (fn []
-         ~@body))))
+;; I sure do hope I don't get memory leaks
+(def id->host* (atom {}))
+(def host->id* (atom {}))
+(def pairs (atom {}))
 
-(defn send-packet [^Writer wr packet]
-  (.write wr (pr-str packet))
-  (.flush wr))
+(defmulti handle-packet
+  (fn [_connection {:keys [type]}] type))
 
-(def hosts (atom {}))
+(defmethod handle-packet :default
+  [connection {:keys [type]}]
+  (send-to connection
+    {:type    :error
+     :message (str "Unknown type: " type)}))
 
-(defn open-session [^Socket connection]
-  (thread
-    (println "Client connected.")
-    (try (with-open [rd (-> connection .getInputStream io/reader LineNumberingPushbackReader.)
-                     wr (-> connection .getOutputStream io/writer)]
-           (while (not (.isClosed connection))
-             (try (let [{:keys [type]
-                         :as   packet} (edn/read rd)]
-                    (if (keyword? type)
-                      (case type
-                        :host (println "Hosting:" (pr-str packet))
-                        :join (println "Joining:" (pr-str packet))
-                        (send-packet wr
-                          {:type    :error
-                           :message "Malformed packet, unrecognized :type!"}))
-                      (send-packet wr
-                        {:type    :error
-                         :message "Malformed packet, no :type given!"})))
-                  (catch RuntimeException e
-                    (send-packet wr
-                      {:type    :error
-                       :class   (.getName (.getClass e))
-                       :message (.getMessage e)})))
-             (recur))
-           (println "Connection closed."))
-         (catch IOException e
-           (.printStackTrace e))
-         (finally (.close connection)))))
+(defmethod handle-packet :host
+  [connection {:keys [id]}]
+  (cond (not id)
+        (send-to connection
+          {:type    :error
+           :message "No host ID given."})
 
-(defn -main [& _args]
-  (let [server (ServerSocket. PORT)]
-    (.addShutdownHook
-      (Runtime/getRuntime)
-      (Thread. ^Runnable (fn [] (.close server))))
-    (while (not (.isClosed server))
-      (try (open-session (.accept server))
-           (catch SocketException e
-             (.printStackTrace e))))))
+        (@id->host* id)
+        (send-to connection
+          {:type    :user-error
+           :message (str "Someone is already hosting the game: " \" id \")})
+
+        (@host->id* connection)
+        (send-to connection
+          {:type    :error
+           :message (str "You are already hosting a game!")})
+
+        (@pairs connection)
+        (send-to connection
+          {:type    :error
+           :message "You are already starting a game!"})
+
+        :else
+        (do (swap! id->host* assoc id connection)
+            (swap! host->id* assoc connection id)
+            (send-to connection
+              {:type    :success
+               :message (str "Hosting game: " \" id \")}))))
+
+(defmethod handle-packet :join
+  [connection {:keys [id]}]
+  (if id
+    (if-let [host-connection (@id->host* id)]
+      (locking connection
+        (cond (= host-connection connection)
+              (send-to connection
+                {:type    :user-error
+                 :message "Cannot join your own game!"})
+
+              (@pairs connection)
+              (send-to connection
+                {:type    :error
+                 :message "You are already starting a game!"})
+
+              (@pairs host-connection)
+              (send-to connection
+                {:type    :error
+                 :message "The host is already starting a game!"})
+
+              :else
+              (do (swap! id->host* dissoc id)
+                  (swap! host->id* dissoc host-connection)
+                  (swap! pairs assoc
+                         connection, host-connection
+                         host-connection, connection)
+                  (send-to connection
+                    {:type :success}))))
+      (send-to connection
+        {:type    :user-error
+         :message (str "Nobody is hosting the game: " \" id \")}))
+    (send-to connection
+      {:type    :error
+       :message "No host ID given."})))
+
+(defmethod handle-packet :offer
+  [connection {:keys [offer]}]
+  (if-let [pair (@pairs connection)]
+    (send-to pair
+      {:type :offer
+       :offer offer})
+    (send-to connection
+      {:type :error
+       :message "Not paired with anyone."})))
+
+(defmethod handle-packet :answer
+  [connection {:keys [answer]}]
+  (if-let [pair (@pairs connection)]
+    (send-to pair
+      {:type :answer
+       :answer answer})
+    (send-to connection
+      {:type :error
+       :message "Not paired with anyone."})))
+
+(defn on-message [connection message]
+  (when-some [packet
+              (try (edn/read-string message)
+                   (catch RuntimeException e
+                     (send-to connection
+                       {:type    :error
+                        :message (str "Malformed EDN: " (.getMessage e))})))]
+    (handle-packet connection packet)))
+
+(defn on-close [connection]
+  (when-let [id (@host->id* connection)]
+    (swap! id->host* dissoc id)
+    (swap! host->id* dissoc connection))
+
+  (when-let [pair (@pairs connection)]
+    (swap! pairs dissoc connection pair)
+    (send-to pair
+      {:type :disconnect})))
+
+(defn -main [& args]
+  (let [port (try (Integer/parseInt (first args))
+                  (catch NumberFormatException _ 4892))
+        server
+        (proxy [WebSocketServer] [(InetSocketAddress. port)]
+          (onClose [connection _code _reason _remote]
+            (on-close connection))
+          (onError [_connection exception]
+            (.printStackTrace exception))
+          (onMessage [connection message]
+            (on-message connection message))
+          (onOpen [connection _handshake]
+            (send-to connection {:type :connected}))
+          (onStart []))]
+    (.start server)
+    (println "Started server on port:" port)
+    ;; await EOF
+    (while (not= -1 (.read System/in)))))
