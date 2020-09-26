4ea0a8a0e100fa4d9662f76cae984cf8ee6f2252
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-09-26 21:15:03 +0100

    Add LICENSE files.

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :sign-releases false}]]
   :clean-targets ^{:protect false} ["target"]
+  :license {:name "MIT"}
   :main kutyagumi.main.gui
   :aot [kutyagumi.main.gui])
